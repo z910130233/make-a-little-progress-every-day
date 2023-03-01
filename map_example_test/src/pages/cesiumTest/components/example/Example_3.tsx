977a0c695cf048f795846f4ae48907dfc44c6c7a
wegis <zelTime@163.com>
2023-03-01 16:15:26 +0800

    官方例子3

--- map_example_test/src/pages/cesiumTest/components/example/Example_3.tsx
+++ map_example_test/src/pages/cesiumTest/components/example/Example_3.tsx
@@ -17,15 +17,27 @@ const Example_3: React.FC = () => {
           -107.919731, 41.003906, -109.04798, 40.998429, -111.047063, 40.998429,
           -111.047063, 42.000709, -111.047063, 44.476286, -111.05254, 45.002073,
         ]),
-        height: 0,
+        height: 200000,
+        extrudedHeight: 250000,
         material: Cesium.Color.RED.withAlpha(0.5),
         outline: true,
         outlineColor: Cesium.Color.BLACK,
       },
     });
 
-    viewer.flyTo(wyoming);
+    // viewer.flyTo(wyoming);
+    test(viewer, wyoming);
   }, [])
+
+  const test = async (viewer: any, wyoming: any): Promise<any> => {
+    const result = await viewer.flyTo(wyoming);
+    if (result) {
+      viewer.selectedEntity = wyoming;
+    }
+
+    wyoming.position = Cesium.Cartesian3.fromDegrees(-107.724, 42.68);
+    viewer.trackedEntity = wyoming;
+  }
 
   return <>
     <div id={"Example_3"} className={style.mapBox}/>
